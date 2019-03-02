30d603918e5a2615ef4db4fb4d192033e0d37caf
Cerina da Graca <dagraca@usc.edu>
2019-03-01 22:39:08 -0800

    hweoir

--- index.php
+++ index.php
@@ -5,11 +5,6 @@
     <title>Uber Shep</title>
   </head>
   <body>
-<<<<<<< HEAD
-    <h2>ashfoiashf</h2>
-=======
-    <h1>Hey Juliette</h1>
-
->>>>>>> ecc9f934da4441e44c30fd2ef0c94d7f3f687dcf
+    hersoiuiiwieoiuwe
   </body>
 </html>

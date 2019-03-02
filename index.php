e16132147bf628581a1e3c2cc0febd63697145cc
J Hill <chirolhi@usc.edu>
2019-03-01 23:45:25 -0800

    distance to goal implemented

--- index.php
+++ index.php
@@ -4,18 +4,34 @@
     <meta charset="utf-8">
     <title>Uber Shep</title>
   </head>
+  <style>
+
+  </style>
   <body>
     <div class="distanceUpdate">
       Move towards your point of meeting.
     </div>
   </body>
   <script>
+    // initialize variables for user's and destination coords
+    let latitude = 0;
+    let longitude = 0;
+    let destLatitude = 34.139137;
+    let destLongitude = -118.125471;
+    let distance = 0;
+
+    // prints every 5 seconds location on console
+    setInterval(function() {
+      getLocation();
+    }, 5000);
+
     // gets location of user from geolocation
     function getLocation() {
   		if ("geolocation" in navigator) {
   			navigator.geolocation.getCurrentPosition(function(position) {
   				latitude = position.coords.latitude
   				longitude = position.coords.longitude
+          distance = getDistanceFromLatLonInM(latitude, longitude, destLatitude, destLongitude);
   				printUserLocation()
   			}, function(error) {
   				alert('Geolocation permission denied. Default location will be set to SAL.')
@@ -30,13 +46,26 @@
     function printUserLocation() {
   		console.log('Latitude: ' + latitude)
   		console.log('Longitude: ' + longitude)
+      console.log('Distance to goal: ' + distance);
   	}
 
-    let latitude = 0;
-    let longitude = 0;
-    setInterval(function() {
-      getLocation();
-    }, 5000);
+    function getDistanceFromLatLonInM(lat1,lon1,lat2,lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = deg2rad(lon2-lon1);
+      var a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      var d = R * c * 1000; // Distance in m
+
+      return d;
+    }
+
+    function deg2rad(deg) {
+      return deg * (Math.PI/180)
+    }
 
   </script>
 </html>

f2bb63bb9fef0294e641826ea074c4fb44536a90
J Hill <chirolhi@usc.edu>
2019-03-01 23:31:22 -0800

    geolocation working

--- index.php
+++ index.php
@@ -5,6 +5,38 @@
     <title>Uber Shep</title>
   </head>
   <body>
-    <h2>Hey soooooophia!</h2>
+    <div class="distanceUpdate">
+      Move towards your point of meeting.
+    </div>
   </body>
+  <script>
+    // gets location of user from geolocation
+    function getLocation() {
+  		if ("geolocation" in navigator) {
+  			navigator.geolocation.getCurrentPosition(function(position) {
+  				latitude = position.coords.latitude
+  				longitude = position.coords.longitude
+  				printUserLocation()
+  			}, function(error) {
+  				alert('Geolocation permission denied. Default location will be set to SAL.')
+  				printUserLocation()
+  			});
+  		} else {
+  			alert('Geolocation is not supported on this browser. Default location will be set to SAL.')
+  			printUserLocation()
+  		}
+  	}
+
+    function printUserLocation() {
+  		console.log('Latitude: ' + latitude)
+  		console.log('Longitude: ' + longitude)
+  	}
+
+    let latitude = 0;
+    let longitude = 0;
+    setInterval(function() {
+      getLocation();
+    }, 5000);
+
+  </script>
 </html>
